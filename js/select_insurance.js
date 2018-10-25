c0628cc1b0f500b622a3085e89c2eb976b1ab059
shao <2210567547@qq.com>
2018-10-25 10:00:39 +0800

    保险管理

--- js/select_insurance.js
+++ js/select_insurance.js
@@ -1,56 +1,34 @@
 $(document).ready(function($) {
-	var W = $(".container").width() - $(".leftList").width() - 45;
-	$(".rightList").width(W);
-	var H = $(".container").height() - $(".con_tit_left").height();
-	$('.con_list_left').height(H);
 
-	$(".con_list_left li").eq(0).addClass('_left_li_active');
-	$('.con_list_right>li').eq(0).show();
-	$(".con_list_left li").click(function(){
-		$('.rightList2').hide();
-		$('.rightList').show();
-		var inx = $(this).index();
-		$(this).addClass('_left_li_active');
-		$(".con_list_left li").not($(this)).removeClass('_left_li_active');
-		$('.con_list_right>li').hide();
-		$('.con_list_right>li').eq(inx).show();
+	$('.select').click(function(e){
+		e.preventDefault();
+		var this_ = $(this);
+		$('.search_list').slideToggle(200,function(){});
+		$('.search_list li').each(function(index, el) {	
+			if($(this).html() ==  this_.html()){
+				$(this).css({background:'#3da8f6',color:'#fff'});
+			}else{
+				$(this).css({background:'#fff',color:'#101010'});
+			}
+		});
+ 
 	})
-	// 点击产品
-	var flag = true;
-	// 选中的数量
-	var con = 0;
-	var str  = con+'/'+$(".con_list_right_select li").length;
-	$('.rightList>p span').eq(1).html(str);
-	$('.con_list_right_select li').click(function(){
-		flag = true;
-		$(this).toggleClass('_pro_li_active');
-		var length = $(this).parent().children('li').length;
-		var activeLength = $(this).parent().children('._pro_li_active').length
-		if(activeLength < length){
-			$(this).parents(".con_list_right_select").prev().children('p').eq(1).children('span').removeClass('_pro_li_active');
-		}else{
-			$(this).parents(".con_list_right_select").prev().children('p').eq(1).children('span').addClass('_pro_li_active');
-		}
-		con = $(".con_list_right_select").find('._pro_li_active').length;
-		str = con+'/'+$(".con_list_right_select li").length;
-		$('.rightList>p span').eq(1).html(str);
-		$('.rightList2>p span').eq(1).html(str);
+	// $('.search_list li').each(function(index, el) {
+	// 	$(this).mouseenter(function(event) {
+	// 		$(this).css({background:'#3da8f6',color:'#fff'});
+	// 	});
+	// 	$(this).mouseleave(function(event) {
+	// 			$(this).css({background:'#fff',color:'#101010'});
+	// 	});
+	// });
+
+	$('.search_list').mouseleave(function(event) {
+		$(this).hide();
+	});
+	$('.search_list li').click(function(){
+		$('.search_list').hide();
+		$('.select').html($(this).html())
+		$('.select').removeClass('color_1');
 	})
-	$('.con_list_right>li p:nth-child(2) span').click(function(){
-		if(flag){
-			$(this).addClass('_pro_li_active');
-			$(this).parents('.con_tit_right').next().find('li').addClass('_pro_li_active');
-			 flag = false;
-		}else{
-			$(this).toggleClass('_pro_li_active');
-			$(this).parents('.con_tit_right').next().find('li').toggleClass('_pro_li_active');
-		}
-		con = $(".con_list_right_select").find('._pro_li_active').length;
-		str = con+'/'+$(".con_list_right_select li").length;
-		$('.rightList>p span').eq(1).html(str);
-		$('.rightList2>p span').eq(1).html(str);
-	})
-	$('.footer button').click(function(){
-		window.location = "index.html";
-	})
+
 });
